7aa645088e38f315a92499b49d0adafcfee46515
jhcc_admin <admin@jhcc.info>
2015-07-27 23:05:42 +0000

    Added ability to manage fax numbers.  Streamlined code

--- modules/communication/fax/ctrl.js
+++ modules/communication/fax/ctrl.js
@@ -1,10 +1,19 @@
 app.lazy.controller('ComFaxCtrl', function($scope, $timeout, $http, $sce, config, FileService, Data, Auth){
 	var acceptedFiles = ['doc', 'docx', 'pdf', 'tif', 'jpg', 'png', 'odt', 'txt', 'html'];
 	
+	var FaxNums		= Data({
+		className: 	'PhoneNumbers',
+		query: 		'?order=-createdAt&where={"type":"fax"}',
+		fireRef:	'FaxNumbers'
+	});
 	var Faxes 		= Data({
 		className: 	'Faxes',
 		query: 		'?order=-createdAt&where={"archived":false}',
 		fireRef:	'Faxes'
+	});
+	
+	$scope.$on(FaxNums.listener, function(e, faxNumbers) {
+		$scope.faxNumbers = faxNumbers
 	});
 	$scope.$on(Faxes.listener, function(e, faxes) {
 		var faxesSent 		= $scope.faxesSent = [];
@@ -18,12 +27,61 @@ app.lazy.controller('ComFaxCtrl', function($scope, $timeout, $http, $sce, config
 	
 	var tools = $scope.tools = {
 		init: function(){
-			tools.file.clear();
+			tools.number.init();
+			tools.file.init();
 			Auth.tools.init().then(function(user){
+				FaxNums.tools.list()
 				Faxes.tools.list()
 			});
 		},
+		number: {
+			init: function(){
+				tools.number.list();
+				tools.number.areaCodes();
+				$scope.number = {status: 'Choose an area code to provision a new fax number.'}
+			},
+			areaCodes: function(){
+				$http.post('https://api.parse.com/1/functions/faxAreaCodes', {}).success(function(data){
+					var ac = data.result.data.data;
+					var acs = [];
+					for(var key in ac)
+						acs.push(key);
+					$scope.areaCodes = acs;
+				}).error(function(e){
+					$scope.file.status = 'Error sending fax.'
+				})
+			},
+			list: function(){
+				FaxNums.tools.broadcast();
+			},
+			add: function(areaCode){
+				FaxNums.tools.save({areaCode: areaCode, type: 'fax'})
+			},
+			focus: function(faxNum){
+				$scope.faxNum = faxNum;
+			},
+			save: function(faxNum){
+				var fax = angular.copy(faxNum);
+				FaxNums.tools.save(faxNum)
+			},
+			remove: function(faxNum){
+				if(confirm('You will not be able to recover this number once it is gone.  Are you sure you want to release this number?')){
+					FaxNums.tools.delete(faxNum).then(function(data){
+						$scope.faxNum = null;
+					})
+				}
+			}
+		},
 		file: {
+			init: function(){
+				$scope.faxPreview = null;
+				var file = {status: 'Choose a file to fax.'}
+				if($scope.file && $scope.file.from)
+					file.from = $scope.file.from;
+				if($scope.file && $scope.file.to)
+					file.to = $scope.file.to;
+				$scope.file = file;
+			},
 			add: function(file){
 				$timeout(function(){ 
 					var pieces = file.attr.name.split('.');
@@ -31,9 +89,11 @@ app.lazy.controller('ComFaxCtrl', function($scope, $timeout, $http, $sce, config
 					file.suffix = pieces[pieces.length-1].toLowerCase();
 					
 					if(acceptedFiles.indexOf(file.suffix) != -1){
-						if($scope.file && $scope.file.number)
-							file.number = $scope.file.number;
 						file.status = 'Uploading File... Please Wait';
+						if($scope.file && $scope.file.from)
+							file.from = $scope.file.from;
+						if($scope.file && $scope.file.to)
+							file.to = $scope.file.to;
 						$scope.file = file;
 						tools.file.upload(file)
 					}else{
@@ -41,13 +101,6 @@ app.lazy.controller('ComFaxCtrl', function($scope, $timeout, $http, $sce, config
 						$scope.file.status = 'You can only send files of type: '+JSON.stringify(acceptedFiles)
 					}
 				});
-			},
-			clear: function(){
-				$scope.faxPreview = null;
-				var file = {status: 'Choose a file to fax.'}
-				if($scope.file && $scope.file.number)
-					file.number = $scope.file.number;
-				$scope.file = file;
 			},
 			upload: function(file) {
 				var src = file.src;
@@ -94,13 +147,18 @@ app.lazy.controller('ComFaxCtrl', function($scope, $timeout, $http, $sce, config
 			},
 			send: function() {
 				var file = $scope.file;
-					file.number = file.number.replace(/\D/g,'');
-				if(file.number.length == 10){
-					var number = 1+''+file.number;
+					file.to = file.to.replace(/\D/g,'');
+				if(file.to.length != 10){
+					alert('You must enter a 10 digit phone number.')
+				}else if(!file.from){
+					alert('You must select the number from which you will be sending this fax.')
+				}else{
+					var number = 1+''+file.to;
 					$scope.file.status = 'Sending...'
 					$http.post('https://api.parse.com/1/functions/sendFax', {
 						to: number,
-						url: file.url
+						url: file.url,
+						from: file.from.number
 					}).success(function(data){
 						$scope.file.status = 'Complete'
 						$timeout(function(){
@@ -109,8 +167,6 @@ app.lazy.controller('ComFaxCtrl', function($scope, $timeout, $http, $sce, config
 					}).error(function(e){
 						$scope.file.status = 'Error sending fax.'
 					})
-				}else{
-					alert('You must enter a 10 digit phone number.')
 				}
 			},
 			archive: function(fax){
