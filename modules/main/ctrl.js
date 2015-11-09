ffedbfc03c2cc70662d232bbcef0e28f0b94beed
Ryan Quinlan <apex2060@site4u2.com>
2015-11-09 17:43:18 +0000

    some formating

--- modules/main/ctrl.js
+++ modules/main/ctrl.js
@@ -1,13 +1,22 @@
 app.lazy.controller('MainCtrl', function($rootScope, $scope, $routeParams, $http, config, Easy, Data){
+	$scope.srvcs = ['Budgeting', 'Educational Support',  'Self-esteem Building', 'Resume Building Skills','Utilities','Rent','Food and Clothing','Basic Life Skills','Heat NM','Safe Link Phones'];
+	$scope.team = [];
+	// 	{name: '', pic: {url: ''}},
+	// 	{name: '', pic: {url: ''}},
+	// 	{name: '', pic: {url: ''}},
+	// 	{name: '', pic: {url: ''}},
+	// ]
 	var tools = $scope.tools = {
 		init: function(){
-			$scope.workers = [{name:{first:'HI'}}];
-			for(var i=0; i<5; i++){
-				$http.get('http://randomuser.me/api/').success(function(person){
-					console.log(person)
-					$scope.workers.push(person);
-				})
-			}
+			for(var i=0; i<6; i++)
+				$.ajax({
+					url: 'https://randomuser.me/api/',
+					dataType: 'json',
+					success: function(data) {
+						$scope.team.push(data.results[0].user);
+						$scope.$apply();
+					}
+				});
 		}
 	}
 	tools.init()
