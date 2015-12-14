5b4ec9a0606f495178822011e1b0baade816ac60
Ryan Quinlan <apex2060@site4u2.com>
2015-12-14 17:16:40 +0000

    Updates w/ real pictures

--- modules/main/ctrl.js
+++ modules/main/ctrl.js
@@ -6,6 +6,40 @@ app.lazy.controller('MainCtrl', function($rootScope, $scope, $routeParams, $http
 	// 	{name: '', pic: {url: ''}},
 	// 	{name: '', pic: {url: ''}},
 	// ]
+
+	var carousel = {
+		type: 'carousel',
+		slides: [{
+			title: '',
+			description: 'Our lovely little office.',
+			img: {
+				src: '/static/images/house.jpg',
+				description: ''
+			}
+		},{
+			title: '',
+			description: 'The Playground.',
+			img: {
+				src: '/static/images/playground.jpg',
+				description: ''
+			}
+		},{
+			title: '',
+			description: 'The children love to drive around.',
+			img: {
+				src: '/static/images/cars.jpg',
+				description: ''
+			}
+		}]
+	}
+	$scope.manifest = [carousel]
+		// {
+		// 	type: 'outpostCards',
+		// 	title: 'Locations',
+		// 	cards: operations
+		// }
+	
+	
 	var tools = $scope.tools = {
 		init: function(){
 			for(var i=0; i<6; i++)
